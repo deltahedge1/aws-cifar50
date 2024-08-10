e48f33faf76f8bc0c0d1f3797873d19616d572cb
deltahedge1 <30775341+deltahedge1@users.noreply.github.com>
2024-08-10 15:40:05 +1000

    transfer all changes

--- src/data.py
+++ src/data.py
@@ -42,14 +42,33 @@ def get_data_loaders(
     # HINT: resize the image to 256 first, then crop them to 224, then add the
     # appropriate transforms for that step
     data_transforms = {
-        "train": transforms.Compose(
+        "train": transforms.Compose([
             # YOUR CODE HERE
-        ),
-        "valid": transforms.Compose(
+            transforms.Resize((256, 256)),
+            transforms.RandomCrop(224),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.ColorJitter(
+                brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1
+            ),
+            transforms.RandomAffine(
+                degrees=30, translate=(0.1, 0.1), scale=(0.8, 1.2), shear=10
+            ),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean, std=std)
+        ]),
+        "valid": transforms.Compose([
             # YOUR CODE HERE
-        ),
+            transforms.Resize((256, 256)),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean, std=std)
+        ]),
         "test": transforms.Compose(
             # YOUR CODE HERE
+            transforms.Resize((256, 256)),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean, std=std)
         ),
     }
 
@@ -58,6 +77,7 @@ def get_data_loaders(
         base_path / "train",
         # YOUR CODE HERE: add the appropriate transform that you defined in
         # the data_transforms dictionary
+        transform=data_transforms["train"]
     )
     # The validation dataset is a split from the train_one_epoch dataset, so we read
     # from the same folder, but we apply the transforms for validation
@@ -65,6 +85,7 @@ def get_data_loaders(
         base_path / "train",
         # YOUR CODE HERE: add the appropriate transform that you defined in
         # the data_transforms dictionary
+        transform=data_transforms["valid"]
     )
 
     # obtain training indices that will be used for validation
@@ -81,7 +102,7 @@ def get_data_loaders(
 
     # define samplers for obtaining training and validation batches
     train_sampler = torch.utils.data.SubsetRandomSampler(train_idx)
-    valid_sampler  = # YOUR CODE HERE
+    valid_sampler  = torch.utils.data.SubsetRandomSampler(valid_idx) # YOUR CODE HERE
 
     # prepare data loaders
     data_loaders["train"] = torch.utils.data.DataLoader(
@@ -93,12 +114,18 @@ def get_data_loaders(
     )
     data_loaders["valid"] = torch.utils.data.DataLoader(
         # YOUR CODE HERE
+        valid_data,
+        batch_size=batch_size,
+        sampler=train_sampler,
+        num_workers=num_workers,
+        **kwargs # Extra keyword arguments are passed to the dataloaders. For example: GPU optimizations.
     )
 
     # Now create the test data loader
     test_data = datasets.ImageFolder(
         base_path / "test",
         # YOUR CODE HERE (add the test transform)
+        transform=data_transforms["test"]
     )
 
     if limit > 0:
@@ -109,6 +136,12 @@ def get_data_loaders(
 
     data_loaders["test"] = torch.utils.data.DataLoader(
         # YOUR CODE HERE (remember to add shuffle=False as well)
+        test_data,
+        batch_size=batch_size,
+        sampler=test_sampler,
+        num_workers=num_workers,
+        shuffle=False,
+        **kwargs # Extra keyword arguments are passed to the dataloaders. For example: GPU optimizations.
     )
 
     return data_loaders
@@ -126,10 +159,10 @@ def visualize_one_batch(data_loaders, max_n: int = 5):
     # YOUR CODE HERE:
     # obtain one batch of training images
     # First obtain an iterator from the train dataloader
-    dataiter  = # YOUR CODE HERE
+    dataiter  = iter(data_loaders["train"]) # YOUR CODE HERE
     # Then call the .next() method on the iterator you just
     # obtained
-    images, labels  = # YOUR CODE HERE
+    images, labels  =  dataiter.next() # YOUR CODE HERE
 
     # Undo the normalization (for visualization purposes)
     mean, std = compute_mean_and_std()
@@ -144,7 +177,7 @@ def visualize_one_batch(data_loaders, max_n: int = 5):
 
     # YOUR CODE HERE:
     # Get class names from the train data loader
-    class_names  = # YOUR CODE HERE
+    class_names = data_loaders["train"].dataset.classes # YOUR CODE HERE
 
     # Convert from BGR (the format used by pytorch) to
     # RGB (the format expected by matplotlib)
--- src/model.py
+++ src/model.py
@@ -1,23 +1,78 @@
 import torch
 import torch.nn as nn
 
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1):
+        super(ConvBlock, self).__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=kernel_size, 
+                padding=padding
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+        
+    def forward(self, x):
+        return self.block(x)
 
 # define the CNN architecture
 class MyModel(nn.Module):
     def __init__(self, num_classes: int = 1000, dropout: float = 0.7) -> None:
 
-        super().__init__()
-
+        super(MyModel, self).__init__()
         # YOUR CODE HERE
         # Define a CNN architecture. Remember to use the variable num_classes
         # to size appropriately the output of your classifier, and if you use
         # the Dropout layer, use the variable "dropout" to indicate how much
         # to use (like nn.Dropout(p=dropout))
+        self.features = nn.Sequential(
+            ConvBlock(in_channels=3, out_channels=64),
+            ConvBlock(in_channels=64, out_channels=64),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            ConvBlock(in_channels=64, out_channels=128),
+            ConvBlock(in_channels=128, out_channels=128),
+            nn.MaxPool2d(kernel_size=2, stride=2), 
 
+            ConvBlock(in_channels=128, out_channels=256),
+            ConvBlock(in_channels=256, out_channels=256),
+            ConvBlock(in_channels=256, out_channels=256),
+            nn.MaxPool2d(kernel_size=2, stride=2), #112
+            
+            ConvBlock(in_channels=256, out_channels=512),
+            ConvBlock(in_channels=512, out_channels=512),
+            ConvBlock(in_channels=512, out_channels=512),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            ConvBlock(in_channels=512, out_channels=512),
+            ConvBlock(in_channels=512, out_channels=512),
+            ConvBlock(in_channels=512, out_channels=512),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+        )
+        
+        self.classifier = nn.Sequential(
+            nn.AdaptiveAvgPool2d((7, 7)),
+            nn.Flatten(),
+            nn.Linear(512 * 7 * 7, 2048),  # Increased to 2048 neurons
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(2048, 1024),  # Added an extra layer with 1024 neurons
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(1024, num_classes)
+        )
+ 
+        
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # YOUR CODE HERE: process the input tensor through the
         # feature extractor, the pooling and the final linear
         # layers (if appropriate for the architecture chosen)
+        x = self.features(x)
+        x = self.classifier(x)
         return x
 
 
